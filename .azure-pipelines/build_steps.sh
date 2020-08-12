8faaed7aa038669f6bf3a603ec0a963f412544a9
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-08-12 10:01:08 +0530

    Update build_steps.sh

--- .azure-pipelines/build_steps.sh
+++ .azure-pipelines/build_steps.sh
@@ -1,19 +1,20 @@
 
 
-#set -xeuo pipefail
-#export PYTHONUNBUFFERED=1
 echo "exporting Mutlidict root directory"
 export MULTIDICT_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
-yum update -y
 echo "Installing build dependencies"
 yum install gcc gcc-c++ python3-devel wget make enchant-devel -y
 cd '/home/multidict_root'
 export PATH='/opt/bin':${PATH}
-echo "Installing requirement"
-/opt/_internal/cpython-$1*/bin/python -m pip install -U setuptools wheel
+export PYTHON=/opt/_internal/cpython-$1*/bin/python
+echo "Update pip"
+result = $PYTHON -m pip install -U setuptools wheel
+echo "$result"
+if result = 1
+ echo "Update pip failed"
+ exit 1
+fi
 echo "Building wheels"
-/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel
-ls dist
 /opt/_internal/cpython-$1*/bin/python setup.py install
 /opt/_internal/cpython-$1*/bin/python -m pip install -r requirements/pytest.txt
 /opt/_internal/cpython-$1*/bin/python -m pip install pytest-azurepipelines
